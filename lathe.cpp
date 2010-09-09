6a7bc44b656d1fc916e03b84c512cd4607c4f052
Alexander Brock <Brock.Alexander@web.de>
2010-09-09 22:21:47 +0200

    Gnuplot export working

--- lathe.cpp
+++ lathe.cpp
@@ -1,9 +1,61 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <list>
 #include <vector>
+#include <cstdlib>
 
 using namespace std;
+
+template<class T>
+class point {
+	public:
+	
+	point() {
+		z = 0.0;
+		x = 0.0;
+	}
+	
+	/*!
+		\brief set the coordinates of the point
+		
+		\param x T x
+		\param z T z
+	*/
+	void set(T z, T x) {
+		(*this).z = z;
+		(*this).x = x;
+	}
+	
+	/*!
+		\brief Set the coordinates to the maximum of the current coordinates and the coordinates of the given point
+		
+		\param comp Point to compare with
+	*/
+	void setMax(point<T>& comp) {
+		z = max(z, comp.z);
+		x = max(x, comp.x);
+	}
+	
+	/*!
+		\brief Set the coordinates to the minimum of the current coordinates and the coordinates of the given point
+		
+		\param comp Point to compare with
+	*/
+	void setMin(point<T>& comp) {
+		z = min(z, comp.z);
+		x = min(x, comp.x);
+	}
+	
+	T x;
+	T z;
+};
+
+template<class T>
+inline ostream& operator<< (ostream& out, const point<T>& p) {
+	return out << p.z << " " << p.x;
+}
+
 
 template<class T>
 class lathe {
@@ -13,17 +65,25 @@ class lathe {
 
 	}
 
-
+	/*!
+		\brief Add a point to either source or destination
+		
+		\param destination true if the point is a point of the destination,
+						false if it belongs to the source shape.
+		\param T z component of the point
+		\param T x component of the point
+	*/
 	void addPoint(bool destination, T z, T x) {
-		vector<T> point;
-		point.reserve(2);
-		point[0] = z;
-		point[1] = x;
+		point<T> newPoint;
+		newPoint.z = z;
+		newPoint.x = x;
+		maximum.setMax(newPoint);
+		minimum.setMin(newPoint);
 		if (destination) {
-			dest.push_back(point);
+			dest.push_back(newPoint);
 		}
 		else {
-			source.push_back(point);
+			source.push_back(newPoint);
 		}
 	}
 
@@ -39,26 +99,78 @@ class lathe {
 		gnuplot(dest, "dest.data");
 	}
 
-	void gnuplot(vector<vector<T> >& points, const char filename[]) {
+	/*!
+		\brief Write a vector of points into a file for
+						visualization using gnuplot.
+						
+		\param points Vector of points
+		\param filename const char[] name of the file to write to.
+	*/
+	void gnuplot(vector<point<T> >& points, const char filename[]) {
 		fstream out;
 		out.open(filename, fstream::out);
-		gnuplot(points, out);
+		if (!out.is_open()) {
+			cout << "file " << filename << "couldn't be opened" << endl;
+			out.close();
+			return;
+		}
+		gnuplot(out, points);
 		out.close();
 	}
 
-	void gnuplot(vector<vector<T> >& points, ostream& out) {
-		for (class vector<vector<T> >::iterator it = points.begin(); it != points.end(); it++) {
-			out << (*it)[0] << " " << (*it)[1] << endl;
+	/*!
+		\brief Call the gnuplot program
+		
+	*/
+	void invokeGnuplot() {
+		stringstream command;
+	
+		command	<< " gnuplot -e \"set terminal svg;"
+						<< "set output 'plot.svg';"
+						<< "set xrange[" << minimum.z - 0.1 << ":" << maximum.z + 0.1 << "];"
+						<< "set yrange[" << maximum.x+0.1 << ":" << minimum.z - 0.1 << "];"
+						<< "plot 'source.data' title 'Source' with lines, 'dest.data' title 'Destination' with lines;\"";
+		system(command.str().c_str());
+		//cout << command.str();
+	} 
+
+	/*!
+		\brief Stuff the contents of a vector of points into a stream
+		
+		\param out stream to stuff the points into
+		\param points vector of points
+	*/
+	void gnuplot(ostream& out, vector<point<T> >& points) {
+		for (class vector<point<T> >::iterator it = points.begin(); it != points.end(); it++) {
+			out << (*it) << endl;
+		}
+	}
+
+	/*!
+		\brief Stuff the contents of a vector into a stream
+		
+		\param out stream to stuff things into
+		\param vec vector
+	*/
+	void vectorOut(ostream& out, vector<T>& vec) {
+		for (class vector<T>::iterator it = vec.begin(); it != vec.end(); it++) {
+			out << (*it) << " ";
 		}
 	}
 
 	private:
 
 	// The shape of the given material
-	vector<vector<T> > source;
+	vector<point<T> > source;
 
 	// The desired shape of the finished part
-	vector<vector<T> > dest;
+	vector<point<T> > dest;
+	
+	// Maximum dimension of source and dest
+	point<T> maximum;
+	
+	// Minimum dimension of source and dest
+	point<T> minimum;
 };
 
 int main(void) {
@@ -67,13 +179,14 @@ int main(void) {
 
 	test.addPoint(true, 0.0, 0.0);
 	test.addPoint(true, 0.0, 1.0);
-	test.addPoint(true, 1.0, 1.0);
+	test.addPoint(true, 1.0, 0.0);
 	test.addPoint(false, 0.0, 0.0);
 	test.addPoint(false, 0.0, 1.0);
 	test.addPoint(false, 1.0, 1.0);
 	test.addPoint(false, 1.0, 0.0);
 
 	test.gnuplot();
+	test.invokeGnuplot();
 
 	return 0;
 }
